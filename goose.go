190dfb3748e38e1d1bf62941c9f5ddc8725a0521
advancedlogic <advancedlogic@me.com>
2014-01-11 15:16:06 +0100

    Update goose.go
    -[add] license
    -[fix] source code cleaned

--- goose.go
+++ goose.go
@@ -1,34 +1,43 @@
+/*
+This is a golang port of "Goose" originaly licensed to Gravity.com
+under one or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.
+
+Golang port was written by Antonio Linari
+
+Gravity.com licenses this file
+to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package goose
 
-func init() {
-	LoadGooseStopwords("resources/text")
-}
-
 type Goose struct {
-	config Configuration
+	config configuration
 }
 
-func NewGoose() Goose {
+func New(args ...string) Goose {
 
 	return Goose{
-		config: GetDefualtConfiguration(),
+		config: GetDefualtConfiguration(args...),
 	}
 }
 
-func GetGoosFromConfiguration(config Configuration) Goose {
-	return Goose{
-		config: config,
-	}
-}
-
-func (gs Goose) ExtractFromUrl(url string) *Article {
-	cc := NewCrawlCandidate(url, "")
-	cc.config = gs.config
+func (this Goose) ExtractFromUrl(url string) *Article {
+	cc := NewCrawler(this.config, url, "")
 	return cc.Crawl()
 }
 
-func (gs Goose) ExtractFromRawHtml(url string, rawHtml string) *Article {
-	cc := NewCrawlCandidate(url, rawHtml)
-	cc.config = gs.config
+func (this Goose) ExtractFromRawHtml(url string, rawHtml string) *Article {
+	cc := NewCrawler(this.config, url, rawHtml)
 	return cc.Crawl()
 }
