e94a9611433969ee372cd692ebaf691a02b9d74b
Antonio Linari <advancedlogic@me.com>
2014-11-19 10:15:48 +0100

    Updated to golang.org/x/net/html

--- outputformatter.go
+++ outputformatter.go
@@ -1,8 +1,8 @@
 package goose
 
 import (
-	"code.google.com/p/go.net/html"
-	"code.google.com/p/go.net/html/atom"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 	"github.com/PuerkitoBio/goquery"
 	"strconv"
 	"strings"
