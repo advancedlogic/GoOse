4de6b91855ab40508bb5d4f949b7f6f6ba0cd490
Nicola Asuni <nicola.asuni@datasift.com>
2015-11-05 17:42:50 +0000

    DEV-4442 improving testing

--- crawler_test.go
+++ crawler_test.go
@@ -17,34 +17,53 @@ func ReadRawHTML(a Article) string {
 	return string(file)
 }
 
-func ValidateArticle(a Article) error {
+func ValidateArticle(expected Article) error {
 	g := New()
-	expected := g.ExtractFromRawHTML(a.FinalURL, ReadRawHTML(a))
+	result := g.ExtractFromRawHTML(expected.FinalURL, ReadRawHTML(expected))
 
-	if expected.Title != a.Title {
-		return fmt.Errorf("article title does not match. Got %q", expected.Title)
+	// fmt.Println("%v\n", result.CleanedText) // DEBUG
+
+	if result.Title != expected.Title {
+		return fmt.Errorf("article title does not match. Got %q", result.Title)
 	}
 
-	if expected.MetaDescription != a.MetaDescription {
-		return fmt.Errorf("article metaDescription does not match. Got %q", expected.MetaDescription)
+	if result.MetaDescription != expected.MetaDescription {
+		return fmt.Errorf("article metaDescription does not match. Got %q", result.MetaDescription)
 	}
 
-	if !strings.Contains(expected.CleanedText, a.CleanedText) {
-		return fmt.Errorf("article cleanedText does not contains %q", a.CleanedText)
+	if !strings.Contains(result.CleanedText, expected.CleanedText) {
+		return fmt.Errorf("article cleanedText does not contains %q", expected.CleanedText)
 	}
 
-	if expected.MetaKeywords != a.MetaKeywords {
-		return fmt.Errorf("article keywords does not match. Got %q", expected.MetaKeywords)
+	if result.MetaKeywords != expected.MetaKeywords {
+		return fmt.Errorf("article keywords does not match. Got %q", result.MetaKeywords)
 	}
-	if expected.CanonicalLink != a.CanonicalLink {
-		return fmt.Errorf("article CanonicalLink does not match. Got %q", expected.CanonicalLink)
+	if result.CanonicalLink != expected.CanonicalLink {
+		return fmt.Errorf("article CanonicalLink does not match. Got %q", result.CanonicalLink)
 	}
 
-	if expected.TopImage != a.TopImage {
-		return fmt.Errorf("article topImage does not match. Got %q", expected.TopImage)
+	if result.TopImage != expected.TopImage {
+		return fmt.Errorf("article topImage does not match. Got %q", result.TopImage)
 	}
 
 	return nil
+}
+
+func Test_ExampleParse(t *testing.T) {
+	article := Article{
+		Domain:          "example.com",
+		Title:           "Example HTML Page TITLE",
+		MetaDescription: "Example page for testing",
+		CleanedText:     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nexample link content\n\nSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		MetaKeywords:    "example,testing",
+		CanonicalLink:   "http://www.example.com/index.html",
+		TopImage:        "",
+	}
+
+	err := ValidateArticle(article)
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func Test_GloboEsporteParse(t *testing.T) {
