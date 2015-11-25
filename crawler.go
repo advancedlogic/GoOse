eec712a4490dcebf250217ef8a1720f931d9bab8
Lorenzo Alberton <l.alberton@quipo.it>
2015-11-25 17:17:01 +0000

    DEV-4510: add newlines around images as they probably act as paragraph separators

--- crawler.go
+++ crawler.go
@@ -130,6 +130,7 @@ func (c Crawler) Crawl() *Article {
 func (c Crawler) addSpacesBetweenTags(text string) string {
 	text = strings.Replace(text, "><", "> <", -1)
 	text = strings.Replace(text, "</blockquote>", "</blockquote>\n", -1)
+	text = strings.Replace(text, "<img ", "\n<img ", -1)
 	return strings.Replace(text, "</p>", "</p>\n", -1)
 }
 
