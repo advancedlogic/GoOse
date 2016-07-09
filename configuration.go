cd96655d7995de0888df4be59d54194817c9f20a
Antonio Linari <advancedlogic@me.com>
2016-07-09 09:57:05 +0200

    Temporary Patch

--- configuration.go
+++ configuration.go
@@ -1,9 +1,6 @@
 package goose
 
-import (
-	"github.com/advancedlogic/gojs-config"
-	"time"
-)
+import "time"
 
 const defaultUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/534.52.7 (KHTML, like Gecko) Version/5.1.2 Safari/534.52.7"
 
@@ -50,29 +47,48 @@ func GetDefaultConfiguration(args ...string) Configuration {
 			timeout:                 time.Duration(5 * time.Second),
 		}
 	}
-
-	path := args[0]
-	jsconfiguration, err := jsconfig.LoadConfig(path)
-	if err != nil {
-		panic(err.Error())
-	}
-	stopWordsPath := jsconfiguration.String("stopWordsPath", "resources/stopwords")
-	stopWords := NewStopwords() //TODO with path
 	return Configuration{
-		localStoragePath:        jsconfiguration.String("localStoragePath", ""), //not used in this version
-		imagesMinBytes:          jsconfiguration.Int("imageMinBytes", 4500),     //not used in this version
-		enableImageFetching:     jsconfiguration.Bool("enableImageFetching", true),
-		useMetaLanguage:         jsconfiguration.Bool("useMetaLanguage", true),
-		targetLanguage:          jsconfiguration.String("targetLanguage", "en"),
-		imageMagickConvertPath:  jsconfiguration.String("imageMagickConvertPath", "/usr/bin/convert"),   //not used in this version
-		imageMagickIdentifyPath: jsconfiguration.String("imageMagickIdentityPath", "/usr/bin/identify"), //not used in this version
-		browserUserAgent:        jsconfiguration.String("browserUserAgent", defaultUserAgent),
-		debug:                   jsconfiguration.Bool("debug", false),
-		extractPublishDate:      jsconfiguration.Bool("extractPublishDate", false),      //TODO
-		additionalDataExtractor: jsconfiguration.Bool("additionalDataExtractor", false), //TODO
-		stopWordsPath:           stopWordsPath,
-		stopWords:               stopWords,
+		localStoragePath:        "",   //not used in this version
+		imagesMinBytes:          4500, //not used in this version
+		enableImageFetching:     true,
+		useMetaLanguage:         true,
+		targetLanguage:          "en",
+		imageMagickConvertPath:  "/usr/bin/convert",  //not used in this version
+		imageMagickIdentifyPath: "/usr/bin/identify", //not used in this version
+		browserUserAgent:        defaultUserAgent,
+		debug:                   false,
+		extractPublishDate:      false,
+		additionalDataExtractor: false,
+		stopWordsPath:           "resources/stopwords",
+		stopWords:               NewStopwords(), //TODO with path
 		parser:                  NewParser(),
-		timeout:                 time.Duration(jsconfiguration.Int("timeout", 5)) * time.Second,
+		timeout:                 time.Duration(5 * time.Second),
 	}
+
+	/*
+		path := args[0]
+		jsconfiguration, err := jsconfig.LoadConfig(path)
+		if err != nil {
+			panic(err.Error())
+		}
+		stopWordsPath := jsconfiguration.String("stopWordsPath", "resources/stopwords")
+		stopWords := NewStopwords() //TODO with path
+		return Configuration{
+			localStoragePath:        jsconfiguration.String("localStoragePath", ""), //not used in this version
+			imagesMinBytes:          jsconfiguration.Int("imageMinBytes", 4500),     //not used in this version
+			enableImageFetching:     jsconfiguration.Bool("enableImageFetching", true),
+			useMetaLanguage:         jsconfiguration.Bool("useMetaLanguage", true),
+			targetLanguage:          jsconfiguration.String("targetLanguage", "en"),
+			imageMagickConvertPath:  jsconfiguration.String("imageMagickConvertPath", "/usr/bin/convert"),   //not used in this version
+			imageMagickIdentifyPath: jsconfiguration.String("imageMagickIdentityPath", "/usr/bin/identify"), //not used in this version
+			browserUserAgent:        jsconfiguration.String("browserUserAgent", defaultUserAgent),
+			debug:                   jsconfiguration.Bool("debug", false),
+			extractPublishDate:      jsconfiguration.Bool("extractPublishDate", false),      //TODO
+			additionalDataExtractor: jsconfiguration.Bool("additionalDataExtractor", false), //TODO
+			stopWordsPath:           stopWordsPath,
+			stopWords:               stopWords,
+			parser:                  NewParser(),
+			timeout:                 time.Duration(jsconfiguration.Int("timeout", 5)) * time.Second,
+		}
+	*/
 }
