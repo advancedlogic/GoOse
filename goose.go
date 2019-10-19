f59f9444807bc2cb6ba65b956f21c374dfbfda2d
Merlin von Trott <mt@fino.digital>
2019-10-19 13:55:17 +0200

    remove not needed Extractors

--- goose.go
+++ goose.go
@@ -32,20 +32,3 @@ func (g Goose) ExtractFromRawHTML(RawHTML string, url string) (*Article, error) 
 	cc := NewCrawler(g.config)
 	return cc.Crawl(RawHTML, url)
 }
-
-// ExtractFromURL follows the URL, fetches the HTML page and returns an article object
-func (g Goose) ExtractFromURLShort(url string) (*Article, error) {
-	HtmlRequester := NewHtmlRequester(g.config)
-	html, err := HtmlRequester.fetchHTML(url)
-	if err != nil {
-		return nil, errors.Wrap(err, "could not get html from from url: "+url)
-	}
-	cc := NewCrawlerShort(g.config)
-	return cc.Crawl(html, url)
-}
-
-// ExtractFromRawHTML returns an article object from the raw HTML content
-func (g Goose) ExtractFromRawHTMLShort(RawHTML string, url string) (*Article, error) {
-	cc := NewCrawlerShort(g.config)
-	return cc.Crawl(RawHTML, url)
-}
